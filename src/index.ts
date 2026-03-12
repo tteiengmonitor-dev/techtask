b145f67e3e16c934d67001b663a134b66248d3ea
tteiengmonitor-dev <ttei.eng.monitor@gmail.com>
2026-03-12 14:33:20 +0700

    Refactor fetch function to handle CSS response

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,41 @@
-import { indexPage } from "./pages/index";
-import { plannerPage } from "./pages/planner";
-import { technicianPage } from "./pages/technician";
+import { indexPage } from "./pages/index"
+import { plannerPage } from "./pages/planner"
+import { technicianPage } from "./pages/technician"
+
+import style from "./css/style.css"
 
 export default {
-  async fetch(request: Request, env: Env) {
 
-    const url = new URL(request.url);
+async fetch(request: Request) {
 
-    if (url.pathname === "/") {
-      return new Response(indexPage(), {
-        headers: { "content-type": "text/html" }
-      });
-    }
+const url = new URL(request.url)
 
-    if (url.pathname === "/planner") {
-      return new Response(plannerPage(), {
-        headers: { "content-type": "text/html" }
-      });
-    }
+if(url.pathname === "/"){
+  return new Response(indexPage(),{
+    headers:{ "content-type":"text/html"}
+  })
+}
 
-    if (url.pathname === "/tech") {
-      return new Response(technicianPage(), {
-        headers: { "content-type": "text/html" }
-      });
-    }
+if(url.pathname === "/planner"){
+  return new Response(plannerPage(),{
+    headers:{ "content-type":"text/html"}
+  })
+}
 
-    return new Response("404", { status: 404 });
-  }
-};
+if(url.pathname === "/tech"){
+  return new Response(technicianPage(),{
+    headers:{ "content-type":"text/html"}
+  })
+}
+
+if(url.pathname === "/css/style.css"){
+  return new Response(style,{
+    headers:{ "content-type":"text/css"}
+  })
+}
+
+return new Response("404",{status:404})
+
+}
+
+}
